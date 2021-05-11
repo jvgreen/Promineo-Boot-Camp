1151a717e26dbbb6efd9eb77aefc962bf2bc6388
Jonah Green <gre11017@byui.edu>
2021-05-10 18:42:42 -0600

    week 4 assignment done

--- mysql/week4/week4.sql
+++ mysql/week4/week4.sql
@@ -24,14 +24,47 @@ begin
 end//
 delimiter ;
 delimiter //
--- loop through the first 10 employees
-create procedure firstTen()
-begin
-	
+
+delimiter //
+-- Determine if an employee gets high medium or low pay
+create procedure getPayLevel()
+begin    
+    select s.salary,
+	case
+		when s.salary > 80000 then "High Salary"
+        when s.salary > 50000 and s.salary < 80000 then "Medium Salary"
+        else "Low Salary"
+	end as 'Pay level'
+    from salaries s
+    inner join employees e
+    on s.emp_no = e.emp_no;
 end//
+delimiter ;
+
+delimiter //
+-- Repeat employee name 
+create procedure repeatName(in empNum int, in repeatNum int)
+begin 
+	select repeat(first_name, repeatNum)
+    from employees
+    where emp_no = empNum;
+end//
+delimiter ;
+
+delimiter //
+	-- return an employees full name
+    create procedure fullName(in empNum int, out fullName varchar(50))
+    begin
+		select concat(first_name, ' ' , last_name)
+        into fullName
+        from employees
+        where emp_no = empNum;
+    end//
+delimiter ;
 
 call replaceEmployee(10001, 'Georgi', 'Facello', 'Tim');
 call getTotalSalary();
-
-drop procedure if exists replaceEmployee;
-drop procedure if exists getTotalSalary;
+call getPayLevel();
+call repeatName(10001, 4);
+call fullName(10001, @out_value);
+select @out_value;
